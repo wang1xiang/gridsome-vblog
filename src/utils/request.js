8fbb262ffa6711ca7665a55c7967e05db77cf46f
wangxiang <wangxiang@szkingdom.com>
2021-04-11 22:16:16 +0800

    token

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ const request = axios.create({
   baseURL: 'https://api.github.com',
   timeout: 60000,
   headers: {
-    Authorization: 'ghp_pnL38cBZqEINelghuCgJWoPQskXVSo4FUX6i',
+    Authorization: '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',
   },
 })
 
